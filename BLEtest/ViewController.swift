e62b108417fa468355c5bb89705c1dec6620a1b0
keisuke519 <s.keisuke519@gmail.com>
2018-06-09 13:54:14 +0900

    first commit

---
+++ BLEtest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BLEtest
+//
+//  Created by 杉浦圭相 on 2018/06/04.
+//  Copyright © 2018年 杉浦圭相. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+
+platform :ios, '11.4'
+use_frameworks!
+
+target 'BLEtest' do
+pod 'AFNetworking', '~> 2.6'
+pod 'ORStackView', '~> 3.0'
+pod 'SwiftyJSON', '~> 2.3'
+pod 'RxBluetoothKit'
+end
